a46b1035faf57ee96f6203de1b3f53c4747e4cae
Famondir <schaefersd@gmail.com>
2021-02-09 09:52:25 +0100

    Added posterior_epred_long function. Both, posterior_eprd_long and posterior_predict_long now root in posterior predictive value_long.

--- R/helpers.R
+++ R/helpers.R
@@ -24,3 +24,78 @@ list2array = function(input.list){  #input a list of dataframes
 
   return(output.array)    # output as a 3-D array
 }
+
+#' Samples from the Posterior Predictive Distribution in long format
+#'
+#' @param model brmsfit
+#' @param n_samples integer
+#'
+#' @return tibble with columns from brmsfit$data (at least original responses, person and item identifier).
+#' Additionally yrep (predicted answers) and .draw (which MCMC draw comes the answer from).
+#' @export
+#'
+#' @examples
+posterior_predict_long <- function(model, n_samples = NULL) {
+  yrep <- posterior_predictive_values_long(model, n_samples, brms::posterior_predict) %>%
+    dplyr::rename(yrep = ppv)
+
+  return(yrep)
+}
+
+#' Expected Values of the Posterior Predictive Distribution in long format
+#'
+#' @param model brmsfit
+#' @param n_samples integer
+#'
+#' @return tibble with columns from brmsfit$data (at least original responses, person and item identifier).
+#' Additionally ppe (posterior predictive estimate) and .draw (which MCMC draw comes the answer from).
+#' @export
+#'
+#' @examples
+posterior_epred_long <- function(model, n_samples = NULL) {
+ppe <- posterior_predictive_values_long(model, n_samples, brms::posterior_epred) %>%
+  dplyr::rename(ppe = ppv)
+  return(ppe)
+}
+
+#' Returns posterior predictive values in long format
+#'
+#' @param model brmsfit
+#' @param n_samples integer
+#' @param f function
+#'
+#' @return tibble with columns from brmsfit$data (at least original responses, person and item identifier).
+#' Additionally y_rep (predicted answers) or ppe (posterior predictive estimate) and .draw (which MCMC draw comes the answer from).
+#'
+#' @examples
+posterior_predictive_values_long <- function(model, n_samples = NULL, f) {
+  draws <- NULL
+  if (!is.null(n_samples)) draws <- sample(1:brms::nsamples(model), size = n_samples, replace = FALSE) %>% sort()
+
+  message('Extracting posterior predictiv values')
+  ppv <- f(model, subset = draws)
+
+  message('Converting responses to long format')
+  ppv <- ppv %>% t() %>% as.data.frame()
+  if (!is.null(draws)) ppv <- ppv %>% setNames(paste0('V', draws))
+  ppv <- ppv  %>% cbind(model$data) %>%
+    tidyr::pivot_longer(names_to = ".draw", names_prefix = 'V', names_transform = as.numeric(), values_to = 'ppv', cols = starts_with("V"))
+
+  return(ppv)
+}
+
+#' Repeat dataframe
+#' Repeats rows in dataframe n times (where n is specified in a vector with equal entries).
+#' Used to lengthen out odds ratio from original responses to match with or dataframe from post for faster substraction.
+#' from Peter Solymos (mefa-package: https://www.rdocumentation.org/packages/mefa/versions/3.2-7/topics/rep.data.frame)
+#'
+#' @param x a matrix or data frame, but can be a vector.
+#' @param ... arguments passed to the function rep, i.e. times, length.out and each (see explanation there).
+#'
+#' @return
+#' @export
+#'
+#' @examples
+rep_dataframe <-  function(x, ...) {
+  as.data.frame(lapply(x, rep, ...))
+}
