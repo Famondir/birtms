d9fef11b8999526cd8faeff772e050aa046f9d89
Famondir <schaefersd@gmail.com>
2021-02-18 15:41:29 +0100

    Now custom CI borders can be passed. Handy if data has some serious outlieres and CI calculation should not be affected from ggplot2::xlim()

--- R/plot_ppmc_ridges.R
+++ R/plot_ppmc_ridges.R
@@ -14,6 +14,7 @@ if(getRversion() >= "2.15.1")  utils::globalVariables(c('quantile', 'xmin', 'xma
 #' @param ... arguments passed to ggridges::stat_density_ridges()
 #' @param hdi boolean; should HDI or equitailed region be plotted
 #' @param ci_width double
+#' @param custom_ci double vector to pass the ci limits manually (e.g. when )
 #'
 #' @return ggplot2 object
 #' @export
@@ -29,17 +30,39 @@ if(getRversion() >= "2.15.1")  utils::globalVariables(c('quantile', 'xmin', 'xma
 #' birtsms::unnest_keep_attr(or_dif_samples) %>%
 #' birtsms::plot_ppmc_distribution(or_dif, group = item2, rope = TRUE)
 #' }
-plot_ppmc_ridges <- function (data, parameter, group = 0, rope = FALSE, hdi = TRUE, ci_width = .89, color = 'lightblue', range = NULL, ...) {
+plot_ppmc_ridges <- function (data, parameter, group = 0, rope = FALSE, hdi = TRUE, ci_width = .89, color = 'lightblue', range = NULL, custom_ci = NULL, ...) {
   hdi_custWidth_internal <- function(...) {
     dots <- list(...)
     hdi_width <- dots[[2]]
     # hdi <- HDInterval::hdi(dots[[1]], credMass = hdi_width, allowSplit = TRUE) # does not split hdi!?
     hdi2 <- tidybayes::mode_hdi(dots[[1]], .width = hdi_width) %>% dplyr::select(ymin, ymax) %>% dplyr::rename(lower = ymin, upper = ymax) %>%
       unlist() %>% sort()
+
     return(hdi2)
   }
 
-  if(hdi) {
+  custom_lims <- function(...) {
+    dots <- list(...)
+    ci <- brms::logit_scaled(head(dots[[2]], -1)) / tail(dots[[2]], 1)
+    ci <- sort(ci)
+
+    return(ci)
+  }
+
+  calc_custom_lims <- function(x) {
+    if(max(abs(x)) > 1) {
+      times <- 1/10^floor(log10(max(abs(x))))
+    } else times <- 1
+
+    custom_ci <- (x*times) %>% sort() %>% brms::inv_logit_scaled()
+
+    return(c(custom_ci, times))
+  }
+
+  if(!is.null(custom_ci)) {
+    ci_width <- calc_custom_lims(custom_ci)
+    qf <- function(...) custom_lims(...)
+  } else if(hdi) {
     qf <- function(...) hdi_custWidth_internal(...)
   } else {
     qf <- function(...) quantile(...)
